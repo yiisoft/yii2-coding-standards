c3789d84e3b96e29926d93b9d1bf44d03ba1040b
Edgard Lorraine Messias <edgardmessias@gmail.com>
2016-11-07 09:11:59 -0200

    Several improvements to PrivatePropertiesUnderscoreSniff (#28)
    * Fixed coding standards
    
    * Added name of property for private properties rule
    
    * Added PHPDoc block

--- Yii2/Sniffs/Properties/PrivatePropertiesUnderscoreSniff.php
+++ Yii2/Sniffs/Properties/PrivatePropertiesUnderscoreSniff.php
@@ -1,22 +1,29 @@
 <?php
 
+/**
+ * Class for a sniff to oblige private property name prefixed with underscore
+ *
+ * This Sniff check if a underscore is missing before private property name.
+ * The private property name is identified by the PHP token T_VARIABLE
+ * The private visibility is identified by the PHP token T_PRIVATE
+ */
 class Yii2_Sniffs_Properties_PrivatePropertiesUnderscoreSniff implements PHP_CodeSniffer_Sniff
 {
-	public function register()
-	{
-		return array(
-			T_PRIVATE,
-		);
-	}
+    public function register()
+    {
+        return [T_PRIVATE];
+    }
 
-	public function process(PHP_CodeSniffer_File $file, $pointer)
-	{
-		$tokens = $file->getTokens();
-		if ($tokens[$pointer]['content'] === 'private' &&
-			$tokens[$pointer + 1]['type'] === 'T_WHITESPACE' &&
-			$tokens[$pointer + 2]['type'] === 'T_VARIABLE' &&
-			strpos($tokens[$pointer + 2]['content'], '$_') !== 0) {
-			$file->addError('Private property name must be prefixed with underscore.', $pointer);
-		}
-	}
+    public function process(PHP_CodeSniffer_File $file, $pointer)
+    {
+        $tokens = $file->getTokens();
+        if ($tokens[$pointer]['content'] === 'private' &&
+            $tokens[$pointer + 1]['type'] === 'T_WHITESPACE' &&
+            $tokens[$pointer + 2]['type'] === 'T_VARIABLE' &&
+            strpos($tokens[$pointer + 2]['content'], '$_') !== 0) {
+
+            $data  = [$tokens[$pointer + 2]['content']];
+            $file->addError('Private property name "%s" must be prefixed with underscore.', $pointer, 'NoUnderscore', $data);
+        }
+    }
 }
