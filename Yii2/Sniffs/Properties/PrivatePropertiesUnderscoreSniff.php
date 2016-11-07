6dc7af55daf66a6cc06bcac64cfa63eccf297a7b
Edgard Lorraine Messias <edgardmessias@gmail.com>
2016-11-07 08:35:21 -0200

    Fixed coding standards

--- Yii2/Sniffs/Properties/PrivatePropertiesUnderscoreSniff.php
+++ Yii2/Sniffs/Properties/PrivatePropertiesUnderscoreSniff.php
@@ -2,21 +2,19 @@
 
 class Yii2_Sniffs_Properties_PrivatePropertiesUnderscoreSniff implements PHP_CodeSniffer_Sniff
 {
-	public function register()
-	{
-		return array(
-			T_PRIVATE,
-		);
-	}
+    public function register()
+    {
+        return [T_PRIVATE];
+    }
 
-	public function process(PHP_CodeSniffer_File $file, $pointer)
-	{
-		$tokens = $file->getTokens();
-		if ($tokens[$pointer]['content'] === 'private' &&
-			$tokens[$pointer + 1]['type'] === 'T_WHITESPACE' &&
-			$tokens[$pointer + 2]['type'] === 'T_VARIABLE' &&
-			strpos($tokens[$pointer + 2]['content'], '$_') !== 0) {
-			$file->addError('Private property name must be prefixed with underscore.', $pointer);
-		}
-	}
+    public function process(PHP_CodeSniffer_File $file, $pointer)
+    {
+        $tokens = $file->getTokens();
+        if ($tokens[$pointer]['content'] === 'private' &&
+            $tokens[$pointer + 1]['type'] === 'T_WHITESPACE' &&
+            $tokens[$pointer + 2]['type'] === 'T_VARIABLE' &&
+            strpos($tokens[$pointer + 2]['content'], '$_') !== 0) {
+            $file->addError('Private property name must be prefixed with underscore.', $pointer);
+        }
+    }
 }
